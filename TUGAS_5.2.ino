ea639bfa70210fb4c84b1414034e0d45f437ac48
anggrian.q <anggrian.mecha@gmail.com>
2016-11-13 09:13:35 -0700

    tugas5.2

---
+++ TUGAS_5.2.ino
@@ -0,0 +1,16 @@
+/* laporan pratikum 5.2 anggrian.q (4211511013) */
+
+void setup() 
+{
+ Serial.begin(115200);
+}
+
+void loop()
+{
+ Serial.write(0xF0);
+ Serial.write(0x1F);
+ Serial.write(0x2F);
+ Serial.write(0xF3);
+ Serial.write(0xF4);
+ delay(2000);
+}
